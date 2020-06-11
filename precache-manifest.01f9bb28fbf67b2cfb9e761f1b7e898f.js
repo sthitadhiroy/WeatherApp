18179f7ddd08e45d48a43ae2b9a2a09dd1095c6d
Sthitadhi Roy <sthitadhiroy@Sthitadhis-MacBook-Air.local>
2020-06-11 12:59:24 +0530

    Updates

---
+++ precache-manifest.01f9bb28fbf67b2cfb9e761f1b7e898f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16858eff673843c8c85d43528c5ed5fe",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "9778ac5e9e6f6c2fc1fd",
+    "url": "/WeatherApp/static/css/main.3883832e.chunk.css"
+  },
+  {
+    "revision": "942c28b60edbe47db9df",
+    "url": "/WeatherApp/static/js/2.30939281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.30939281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9778ac5e9e6f6c2fc1fd",
+    "url": "/WeatherApp/static/js/main.8c94b8ed.chunk.js"
+  },
+  {
+    "revision": "44b413d9e193adaf6443",
+    "url": "/WeatherApp/static/js/runtime-main.6a8a7fa9.js"
+  }
+]);
